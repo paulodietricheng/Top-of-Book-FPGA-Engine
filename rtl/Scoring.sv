9cb27c088e418f02a026527ed6120d4d0992fbd3
Paulo DIetrich <paulo.dietrich.eng@gmail.com>
2026-03-19 11:59:38 -0500

    Update Scoring.sv

--- rtl/Scoring.sv
+++ rtl/Scoring.sv
@@ -22,18 +22,30 @@
 import Data_Structures::*;
 
 module Scoring(
-        // Upstream
-        input quote_t in_quote_c,
+        input clk, rst_n, // Signals
+        input quote_t in_quote, in_quote_c, // Upstream
         // Downstream
-        output score_t out_score 
+        output score_t out_score, 
+        output quote_t out_quote
     );
     
-    always_comb begin
-        out_score.valid = in_quote_c.valid;
-        out_score.price = in_quote_c.price;
-        out_score.timestamp = in_quote_c.timestamp;
-        out_score.lane_id = in_quote_c.lane_id;   
-        out_score.size = in_quote_c.size;         
+    score_t reg_score;
+    quote_t reg_quote;
+    
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            reg_score <= '0;
+            reg_quote <= '0;
+        end else if(in_quote_c.valid) begin
+            reg_score.valid <= in_quote_c.valid;
+            reg_score.price <= in_quote_c.price;
+            reg_score.timestamp <= in_quote_c.timestamp;
+            reg_score.lane_id <= in_quote_c.lane_id;            
+            reg_quote <= in_quote;
+        end
     end
+    
+    assign out_score = reg_score;
+    assign out_quote = reg_quote;
     
 endmodule
