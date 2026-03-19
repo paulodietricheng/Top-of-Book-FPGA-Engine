38fa7e7d04d84c40da4770a4f44b8552b306d1f2
Paulo DIetrich <paulo.dietrich.eng@gmail.com>
2026-03-19 11:59:14 -0500

    Update Module_Arbiter.sv

--- rtl/Module_Arbiter.sv
+++ rtl/Module_Arbiter.sv
@@ -24,11 +24,21 @@ import Data_Structures::*;
 module Module_Arbiter(        
         // Upstream
         input score_t in_score_A, in_score_B,
+        input quote_t in_quote_A, in_quote_B,
         
         // Donwstream
+        output quote_t winner_quote,
         output score_t winner_score
     );
     
-    assign winner_score = (in_score_A[99:32] > in_score_B[99:32]) ? in_score_A : in_score_B;
-  
+    always_comb begin
+        if(in_score_A >= in_score_B) begin
+            winner_quote = in_quote_A;
+            winner_score = in_score_A;
+        end else begin
+            winner_quote = in_quote_B;
+            winner_score = in_score_B;
+        end
+    end
+    
 endmodule
