3ad0c8eab30ae2977354e903d0f770c6ed99684f
Paulo DIetrich <paulo.dietrich.eng@gmail.com>
2026-03-19 11:57:53 -0500

    Upload baseline Decoder.sv

--- rtl/Decoder.sv
+++ rtl/Decoder.sv
@@ -18,7 +18,10 @@ module Decoder #(
     parameter int TIMESTAMP_W = 32,
     parameter int SIZE_W = 32,
     parameter int LANE_W = 3
-    )(        
+    )(
+        input logic clk,
+        input logic rst_n,
+        
         // Upstream
         input logic [RAW_DATA_W-1:0] in_data,
         input logic [LANE_W-1:0] lane_id,
@@ -35,15 +38,25 @@ module Decoder #(
     localparam SIZE_START = TIME_END - 1;
     localparam SIZE_END = SIZE_START - (SIZE_W - 1);
     localparam LANE_START = SIZE_END - 1;
-    
-    always_comb begin
-        out_quote.valid = in_data[VALID_BIT];
-        out_quote.side = in_data[SIDE_BIT];
-        out_quote.price = in_data[PRICE_START:PRICE_END];
-        out_quote.timestamp = in_data[TIME_START:TIME_END];
-        out_quote.size = in_data[SIZE_START:SIZE_END];
-        out_quote.lane_id = lane_id;
+
+    // Register to hold decoded fields
+    quote_t reg_quote;
+
+    // Decoding
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            reg_quote <= '0;
+        end else begin
+            reg_quote.valid <= in_data[VALID_BIT];
+            reg_quote.side <= in_data[SIDE_BIT];
+            reg_quote.price <= in_data[PRICE_START:PRICE_END];
+            reg_quote.timestamp <= in_data[TIME_START:TIME_END];
+            reg_quote.size <= in_data[SIZE_START:SIZE_END];
+            reg_quote.lane_id <= lane_id;
+        end
     end
+
+    // Outputs
+    assign out_quote = reg_quote;
     
 endmodule
-
